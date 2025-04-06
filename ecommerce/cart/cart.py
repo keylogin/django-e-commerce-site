c8ca027790ecd19ea0ac47fdff33c0e70ca02a60
keylogin <seriousam_007@yahoo.com>
2025-04-06 18:56:23 +0300

    updates

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -4,16 +4,37 @@ class Cart():
         
         self.session = request.session
         
-        # Returning user - obtain his/her existing session
+        # Returning user - obtain his/her existing session_key
         
         cart = self.session.get("session_key")
         
         
-        # New user - generate a new session
+        # New user - generate a new session_key
         
         if 'session_key' not in request.session:
             
-            cart = self.session["session_key"] = {"fav_number": 13}
+            cart = self.session["session_key"] = {}
             
         self.cart = cart
         
+
+
+    def add(self, product, product_quantity):
+        
+        product_id = str(product.id)
+        
+        if product_id in self.cart:
+            
+            self.cart[product_id]['qty'] = int(product_quantity)
+            
+        else:
+            
+            self.cart[product_id] = {'price': str(product.price), 
+                                    'qty': int(product_quantity)}
+            
+        self.session.modified = True
+        
+        
+    def __len__(self):
+        
+        return sum(item['qty'] for item in self.cart.values())
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,22 +1,47 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .cart import Cart
+from store.models import Product
+from django.shortcuts import get_object_or_404
 
 def cart_summary(request):
-    
     return render(request, "cart/cart-summary.html")
 
-
 def cart_add(request):
-    
-    pass
+    try:
+        cart = Cart(request)
+        print(type(cart))
+        
+        if request.POST.get('action') == 'post':
+            product_id = int(request.POST.get('product_id'))
+            product_quantity = int(request.POST.get('product_quantity'))
 
+            product = get_object_or_404(Product, id=product_id)
+            
+            cart.add(product=product, product_quantity=product_quantity)
+
+
+            cart_quantity = cart.__len__()
+            
+            
+            response = JsonResponse({'quantity': cart_quantity})
+            return response
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
 
 def cart_delete(request):
-    
-    pass
-
+    try:
+        cart = Cart(request)
+        # implement logic to delete product from cart
+        pass
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
 
 def cart_update(request):
-    
-    pass
+    try:
+        cart = Cart(request)
+        # implement logic to update product quantity in cart
+        pass
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
